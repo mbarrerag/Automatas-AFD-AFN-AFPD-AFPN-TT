e416d88f6d317c4a75737954653d359d7f1201fd
cesarArthuroLemos <clemos@unal.edu.co>
2023-05-28 01:40:03 -0500

    Primeros, pero bonitos pasos

---
+++ AFN-Lambda.py
@@ -0,0 +1,137 @@
+from time import sleep
+
+class AFN_Lambda:
+    def __init__(self, alfabeto=None, estados=None, estadoInicial=None, estadosAceptacion=None, delta=None,
+                 nombreArchivo=None):
+        if nombreArchivo:
+            self.cargarDesdeArchivo(nombreArchivo)
+
+        else:
+            self.alfabeto = alfabeto
+            self.estados = estados
+            self.estadoInicial = estadoInicial
+            self.estadosAceptacion = estadosAceptacion
+            self.delta = delta
+            self.estadosLimbo = []
+            self.estadosInaccesibles = []
+
+
+    def cargarDesdeArchivo(self, nombreArchivo):
+        self.alfabeto = []
+        self.estados = []
+        self.estadoInicial = None
+        self.estadosAceptacion = []
+        # Guardaremos las transiciones desde cada estado en diccionarios,
+        # donde la clave es la letra, y el valor es el destino
+        self.delta: list[dict] = []
+        self.estadosInaccesibles = []
+
+        with open(nombreArchivo, 'r') as f:
+            lines = f.readlines()
+
+            for i in range(len(lines)):
+                if lines[i].strip() == '#alphabet':
+                    current = lines[i+1]
+                    j = i+1
+                    while current.strip() != "#states":
+                        if current.strip().__contains__("-"):
+                            start, end = current.split('-')
+                            start = start.strip()
+                            end = end.strip()
+                            lettersRange = [chr(x) for x in range(ord(start), ord(end)+1)]
+                            for letter in lettersRange:
+                                self.alfabeto.append(letter)
+                        else:
+                            self.alfabeto.append(current.strip())
+                        j += 1
+                        current = lines[j].strip()
+
+                if lines[i].strip() == '#states':
+                    while lines[i + 1].strip() != '#initial':
+                        self.estados.append(lines[i + 1].strip())
+                        i += 1
+
+                if lines[i].strip() == '#initial':
+                    self.estadoInicial = lines[i + 1].strip()
+                    i += 1
+
+                if lines[i].strip() == '#accepting':
+                    while lines[i + 1].strip() != '#transitions':
+                        self.estadosAceptacion.append(lines[i + 1].strip())
+                        i += 1
+
+                if lines[i].strip() == '#transitions':
+                    # Primero, llenaremos cada índice de delta con diccionarios
+                    for estado in range(0, len(self.estados)):
+                        self.delta.append({})
+
+                    while i < len(lines) and lines[i + 1].strip() != '':
+                        source, letter = lines[i + 1].strip().split(':')
+                        letter, targets = letter.split('>')
+                        state = self.estados.index(source)
+                        if ';' not in targets:
+                            self.delta[state][letter] = targets
+                        else:
+                            targets = targets.split(';')
+                            self.delta[state][letter] = targets
+                        i += 1
+
+    def hallarEstadosInaccesibles(self):
+        initialState = self.estados.index(self.estadoInicial)
+        isAccesible = [False] * len(self.estados)  # Lista donde registramos cuáles estados son accesibles y cuáles no
+        isAccesible[initialState] = True  # Es decir, indicamos que el estado inicial es accesible
+
+        # Tenemos que recorrer el autómata para determinar cuáles estados son accesibles y cuáles no
+        # Empezamos en el estado inicial. Todos los estados a los cuales se puede acceder partiendo desde este estado
+        # son accesibles.
+        # Entonces examinamos cada uno de ellos. Nuevamente, los estados accesibles por estos estados
+        currentIteration = [initialState]
+
+        allInaccesibleDetected = False
+
+        while not allInaccesibleDetected:
+            auxCurrentIteration = currentIteration.copy()
+            currentIteration = []
+            for state in auxCurrentIteration:
+
+                newAccesibleStates = []
+                targets = list(self.delta[state].values())  # Hallamos los estados a los que hay transiciones desde este estado
+
+                for target in targets:
+                    if type(target) == list:  # Si se trata de una lista de estados, se deben individualizar
+                        for individualState in target:
+                            newAccesibleStates.append(individualState)
+                    else:
+                        newAccesibleStates.append(target)
+
+                for accesibleState in newAccesibleStates:
+                    stateIndex = self.estados.index(accesibleState)
+                    if not isAccesible[stateIndex]:
+                        isAccesible[stateIndex] = True
+                        currentIteration.append(stateIndex)
+            if len(currentIteration) == 0:
+                allInaccesibleDetected = True
+
+        return isAccesible
+
+
+
+
+
+
+
+
+
+
+
+firstAFNL = AFN_Lambda(nombreArchivo="firstAFNLtest.NFE")
+print(firstAFNL.alfabeto)
+print(firstAFNL.estados)
+print(firstAFNL.estadoInicial)
+print(firstAFNL.estadosAceptacion)
+print(firstAFNL.delta)
+
+print(firstAFNL.hallarEstadosInaccesibles())
+
+
+
