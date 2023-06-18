ffded6018ee4181825296a2422d1c2f7e6c14c5f
Cuanse <tiquejuan10@hotmail.com>
2023-06-17 19:40:18 -0500

    AFD error lectura archivos exportados previamente
    creo que #limbo en el archivo exportado genera error para leer el archivo

--- Pruebas.py
+++ Pruebas.py
@@ -12,7 +12,8 @@ class ClasePrueba:
     
     def probarAFD(self):
         # Crear autómatas AFD
-        afd1 = AFD(nombreArchivo='AFDParAParB.txt')
+        afd1 = AFD(nombreArchivo='resultado_con_detalles.txt')
+        afd1.verificarCorregirCompletitud()
         #afd1 = AFD(afd1.alfabeto,afd1.estados,afd1.estadoInicial,afd1.estadosAceptacion,afd1.delta)
         # Procesar cadenas con y sin detalles
         cadena = 'aba'
@@ -119,9 +120,10 @@ class ClasePrueba:
         afd1_simplificado.procesar_cadena(cadena)
 
     def generar_cadenas_afn(self,afns):
+        #Me gustaría que se generaran cadenas dependiendo del lenguaje de los afns (no todos tienen de lenguaje {a,b}) para poder hacer más pruebas
         cadenas_generadas = []
         for afn in afns:
-            for _ in range(5000):
+            for _ in range(2):
                 tamano = random.randint(1, 10)  # Choose the size randomly
                 cadena = ''.join(random.choices(['a', 'b'], k=tamano))  # Generate a random string
                 print(cadena)
@@ -130,7 +132,11 @@ class ClasePrueba:
     
     def validarAFNtoAFD(self):
          afn1 = AFN(nombreArchivo='testAFN.NFA') 
-         afns =[afn1]
+         afd2 = AFD(nombreArchivo='evenA.DFA')
+         afn3 = AFN(nombreArchivo='testAFN.NFA')  
+         afn4 = AFN(nombreArchivo='conversionAFNtoAFDTest.txt')
+         afn5 = AFN(nombreArchivo='AFNTest - copia.txt') 
+         afns =[afn1,afd2,afn3,afn4,afn5]
          lista_cadenas = clase_prueba.generar_cadenas_afn(afns)
          imprimir_pantalla = True
          print("Procesamiento AFN")
@@ -190,9 +196,9 @@ class ClasePrueba:
 
 # Crear instancia de la clase ClasePrueba y ejecutar los método correspondiente
 clase_prueba = ClasePrueba()
-#clase_prueba.probarAFD()
+clase_prueba.probarAFD()
 #clase_prueba.ProbarAFN()
-clase_prueba.probarAFNtoAFD()
+#clase_prueba.probarAFNtoAFD()
 #clase_prueba.probarComplemento()
 #clase_prueba.probarProductoCartesiano()
 #clase_prueba.probarSimplificacion()
