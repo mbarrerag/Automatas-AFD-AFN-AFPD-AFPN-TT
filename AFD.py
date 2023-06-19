b3e9f124341eebe73789043fc2d1f0064054278f
XdmriveroeX <104804272+XdmriveroeX@users.noreply.github.com>
2023-06-19 18:05:40 -0500

    se pasaron los tests al main

--- AFD.py
+++ AFD.py
@@ -1,5 +1,5 @@
 from graphviz import Digraph
-#aaa
+
 class AFD:
     def __init__(self, alfabeto=None, estados=None, estadoInicial=None, estadosAceptacion=None, delta=None, nombreArchivo=None):
         if nombreArchivo:
@@ -399,6 +399,7 @@ class AFD:
 
     def draw(self):
         dfa = Digraph()
+        dfa.attr(rankdir='LR')
 
         for estado in self.estados:
             if estado in self.estadosAceptacion:
@@ -495,107 +496,5 @@ class AFD:
                 self.estadoInicial = self.combinar_estados(cluster)
             if any(estado in self.estadosAceptacion for estado in cluster):
                 self.estadosAceptacion = [self.combinar_estados(cluster) for cluster in clusters if any(estado in self.estadosAceptacion for estado in cluster)]
-
-
-def main():
-    print('\nPruebas de la clase AFD\n')
-
-    # print("Revisar si el AFD esta completo y corregirlo")
-
-    # alfabeto = ['a', 'b']
-    # estados = ['s0', 's1']
-    # estadoInicial = 's0'
-    # estadosAceptacion = ['s0']
-    # delta = {'s0': {'a': 's1'}, 's1': {'a': 's0', 'b': 's1'}}
-
-    # automata = AFD(alfabeto=alfabeto, 
-    #            estados=estados, 
-    #            estadoInicial=estadoInicial, 
-    #            estadosAceptacion=estadosAceptacion, 
-    #            delta=delta)
-    
-    # automata.verificarCorregirCompletitud()
-    # print(automata) 
-
-    # print('Construir a partir de archivo\n')
-    # automata = AFD(nombreArchivo='./Automatas_AFD/Automata_Incompleto.DFA')
-    # print(automata)
-
-    # print('Hallar estados limbo\n')
-    # automata = AFD(nombreArchivo='./Automatas_AFD/Estados_Limbo.DFA')
-    # print("Estados limbo: ", automata.hallarEstadosLimbo())
-
-    # print('Hallar estados inaccesibles\n')
-    # automata = AFD(nombreArchivo='./Automatas_AFD/Estados_Inaccesibles.DFA')
-    # print("Estados inaccesibles: ", automata.hallarEstadosInaccesibles())
-
-    # print('Imprimir automata simplificado y completo\n')
-    # automata = AFD(nombreArchivo='./Automatas_AFD/Automata_Incompleto.DFA')
-    # print(automata.imprimirAFDSimplificado())
-
-    # print('Exportar a archivo\n')
-    # automata = AFD(nombreArchivo='./Automatas_AFD/Automata_Incompleto.DFA')
-    # automata.exportar('./Automatas_AFD/testAFDexport.DFA')
-
-    # print('Procesar en detalle\n')
-    # automata = AFD(nombreArchivo='./Automatas_AFD/EvenAs.DFA')
-    # cadena = 'aabbaab'
-    # print("\n", automata.procesar_cadena_con_detalles(cadena), "\n")
-
-    # print('Procesar Lista de Cadenas\n')
-    # automata = AFD(nombreArchivo='./Automatas_AFD/EvenAs.DFA')
-    # listaCadenas = ['aabbaab', 'aabbaa', 'aabbaaba']
-    # print("\n", automata.procesarListaCadenas(listaCadenas, None, True), "\n")
-
-    # print('Hallar complemento\n')
-    # automata = AFD(nombreArchivo='./Automatas_AFD/EvenAs.DFA')
-    # print(automata.hallarComplemento())
-
-    # print("Hallar ProductorCartesianoY\n")
-    # automata1 = AFD(nombreArchivo='./Automatas_AFD/EvenAs.DFA')
-    # automata2 = AFD(nombreArchivo='./Automatas_AFD/EvenBs.DFA')
-    # CartesianoY = automata1.hallarProductoCartesianoY(automata1, automata2)
-    # cadena = "baabbaab"
-    # print(CartesianoY)
-    # print(CartesianoY.procesar_cadena_con_detalles(cadena))
-
-    # print("Hallar ProductoCartesianoO\n")
-    # automata1 = AFD(nombreArchivo='./Automatas_AFD/EvenAs.DFA')
-    # automata2 = AFD(nombreArchivo='./Automatas_AFD/EvenBs.DFA')
-    # CartesianoO = automata1.hallarProductoCartesianoO(automata1, automata2)
-    # cadena = "aabbab"
-    # print(CartesianoO)
-    # print(CartesianoO.procesar_cadena_con_detalles(cadena))
-
-    # print("Hallar ProductoCartesianoDiferencia\n")
-    # automata1 = AFD(nombreArchivo='./Automatas_AFD/EvenAs.DFA')
-    # automata2 = AFD(nombreArchivo='./Automatas_AFD/EvenBs.DFA')
-    # CartesianoDif = automata1.hallarProductoCartesianoDiferencia(automata1, automata2)
-    # cadena = "aaabbbab"
-    # print(CartesianoDif)
-    # print(CartesianoDif.procesar_cadena_con_detalles(cadena))
-
-    # print("Hallar ProductoCartesianoDiferenciaSimetrica\n")
-    # automata1 = AFD(nombreArchivo='./Automatas_AFD/EvenAs.DFA')
-    # automata2 = AFD(nombreArchivo='./Automatas_AFD/EvenBs.DFA')
-    # CartesianoDifSim = automata1.hallarProductoCartesianoDiferenciaSimetrica(automata1, automata2)
-    # cadena = "aaabbbb"
-    # print(CartesianoDifSim)
-    # print(CartesianoDifSim.procesar_cadena_con_detalles(cadena))
-
-    # print("Simplificar\n")
-    # automata = AFD(nombreArchivo='./Automatas_AFD/Min_Test.DFA')
-    # automata.simplificarAFD()
-    # print(automata)
-
-
-main()
-       
-
-
-
-
-
-
 
 
